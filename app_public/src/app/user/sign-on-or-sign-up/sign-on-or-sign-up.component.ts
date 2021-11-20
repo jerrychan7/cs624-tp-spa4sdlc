b58f423668bdcf2fc828dcf75c1233f6c9a75eaf
pulafei <1847813772@qq.com>
2021-11-19 20:11:27 -0800

    add user login and registration interface

---
+++ app_public/src/app/user/sign-on-or-sign-up/sign-on-or-sign-up.component.ts
@@ -0,0 +1,145 @@
+import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-sign-on-or-sign-up',
+  templateUrl: './sign-on-or-sign-up.component.html',
+  styleUrls: ['./sign-on-or-sign-up.component.css'],
+  animations: [
+    trigger('cardFlip', [
+      state('signIn', style({
+        transform: 'none'
+      })),
+      state('signUp', style({
+        transform: 'rotateY(180deg)'
+      })),
+      transition('signIn => signUp', animate('500ms ease-out')),
+      transition('signUp => signIn', animate('500ms ease-in'))
+    ])
+  ]
+})
+export class SignOnOrSignUpComponent implements OnInit {
+
+  constructor(
+    public translate: TranslateService,
+    private formBuilder: FormBuilder,
+    private usrService: UserService,
+  ) {
+    translate.setTranslation("en", {
+      "sign_in_or_up": {
+        "sign_in": "Sign in",
+        "sign_up": "Sign up",
+        "username": "Username",
+        "password": "Password",
+        "confirmPwd": "Confirm password",
+        "email": "Email",
+        "confirm": "Confirm",
+        "error": {
+          "username": {
+            "required": "Username is <b>required</b>.",
+          },
+          "password": {
+            "required": "Password is <b>required</b>.",
+            "minlength": "Too short.",
+          },
+          "confirmPwd": {
+            "required": "Confirm password is <b>required</b>.",
+            "mustMatch": "Does not match the password.",
+          },
+          "email": {
+            "not_valid": "Please enter a valid email address.",
+            "required": "Email is <b>required</b>.",
+          },
+        },
+      },
+    }, true);
+    translate.setTranslation("zh_cn", {
+      "sign_in_or_up": {
+        "sign_in": "登录",
+        "sign_up": "注册",
+        "username": "用户名",
+        "password": "密码",
+        "confirmPwd": "确认密码",
+        "email": "邮箱",
+        "confirm": "确认",
+        "error": {
+          "username": {
+            "required": "用户名是<b>必须</b>的。",
+          },
+          "password": {
+            "required": "密码是<b>必须</b>的。",
+            "minlength": "太短了。",
+          },
+          "confirmPwd": {
+            "required": "该字段是<b>必须</b>的。",
+            "mustMatch": "和原密码不匹配。",
+          },
+          "email": {
+            "not_valid": "请输入一个合法的邮箱地址。",
+            "required": "邮箱是<b>必须</b>的。",
+          }
+        },
+      },
+    }, true);
+  }
+
+  cardFlipState = 'signIn';
+  passwordHide = true;
+  toggleFlip() {
+    this.cardFlipState = this.cardFlipState == 'signIn'? 'signUp': 'signIn';
+    this.passwordHide = true;
+    this.initOrResetFormGroup();
+  }
+
+  private ComparePassword(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors["mustMatch"]) {
+        return;
+      }
+
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ mustMatch: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    };
+  }
+
+  signInFormGroup: FormGroup = this.formBuilder.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+  signUpFormGroup: FormGroup = this.formBuilder.group({
+    username: ['', Validators.required],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPwd: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+  }, {
+    validator: this.ComparePassword("password", "confirmPwd"),
+  });
+  private initOrResetFormGroup() {
+    this.signInFormGroup.reset();
+    this.signUpFormGroup.reset();
+  }
+
+  onSignIn() {
+    const {username, password} = this.signInFormGroup.getRawValue();
+    console.log({username, password});
+  }
+  onSignUp() {
+    const {username, password, email} = this.signUpFormGroup.getRawValue();
+    console.log({username, password, email});
+  }
+
+
+  ngOnInit(): void {
+    this.initOrResetFormGroup();
+  }
+
+}
--- app_public/src/app/user/user.module.ts
+++ app_public/src/app/user/user.module.ts
@@ -3,22 +3,43 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { UserComponent } from "./user.component";
+import { SignOnOrSignUpComponent } from './sign-on-or-sign-up/sign-on-or-sign-up.component';
 
 
 @NgModule({
   declarations: [
     UserComponent,
+    SignOnOrSignUpComponent,
   ],
   imports: [
     CommonModule,
     RouterModule.forChild([{
-      path: '', pathMatch: 'full', component: UserComponent,
+      path: '', pathMatch: 'full', component: SignOnOrSignUpComponent,
+    }, {
+      path: ":usrID",
+      component: UserComponent,
     }, ]),
     TranslateModule.forChild({
       defaultLanguage: "en",
     }),
-  ]
+
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule, ReactiveFormsModule,
+  ],
+  providers: [
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'legacy'}}
+  ],
 })
 export class UserModule { }
